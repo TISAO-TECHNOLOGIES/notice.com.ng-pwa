3b2cf480c78edd772180a013d21e2ac039f021f7
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2023-01-19 15:46:16 +0100

    update react-router links on Navigation component and navdata

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { linkData, socialLinks } from "../data/nav";
 
 const Navigation = () => {
 	useEffect(() => {
@@ -44,164 +46,28 @@ const Navigation = () => {
 		<>
 			<nav className="navbar navbar-expand-lg fixed-top navbar-scroll">
 				<div className="container-fluid">
-					{/* <!--logo--> */}
+					{/* LOGO*/}
 					<div className="logo">
-						<a href="./index.html">
+						<NavLink to="/">
 							<img src="/src/assets/img/logo-dark.png" alt="" className="logo-dark" />
 							<img src="/src/assets/img/logo-white.png" alt="" className="logo-white" />
-						</a>
+						</NavLink>
 					</div>
-					{/* <!--/--> */}
 
-					{/* <!--navbar-collapse--> */}
+					{/* NAVBAR-COLLAPSE */}
 					<div className="collapse navbar-collapse" id="main_nav">
 						<ul className="navbar-nav ml-auto mr-auto">
-							<li className="nav-item dropdown">
-								<a className="nav-link dropdown-toggle active" href="./index.html" data-toggle="dropdown">
-									{" "}
-									Home{" "}
-								</a>
-								<ul className="dropdown-menu fade-up">
-									<li>
-										<a className="dropdown-item active" href="./index.html">
-											Demo 1
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./index-2.html">
-											Demo 2{" "}
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./index-3.html">
-											Demo 3{" "}
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./index-4.html">
-											Demo 4{" "}
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./index-5.html">
-											Demo 5
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./index-6.html">
-											Demo 6{" "}
-										</a>
-									</li>
-								</ul>
-							</li>
-
-							<li className="nav-item dropdown">
-								<a className="nav-link  dropdown-toggle" href="./blog-grid.html" data-toggle="dropdown">
-									{" "}
-									Blog{" "}
-								</a>
-								<ul className="dropdown-menu fade-up">
-									<li>
-										<a className="dropdown-item" href="./blog-grid.html">
-											{" "}
-											Blog grid
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./blog-masonry.html">
-											{" "}
-											Blog masonry{" "}
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./blog-list.html">
-											{" "}
-											Blog list{" "}
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li className="nav-item dropdown">
-								<a className="nav-link  dropdown-toggle" href="./post-default.html" data-toggle="dropdown">
-									{" "}
-									Posts Features{" "}
-								</a>
-								<ul className="dropdown-menu fade-up">
-									<li>
-										<a className="dropdown-item" href="./post-default.html">
-											{" "}
-											post default
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./post-video.html">
-											{" "}
-											post video
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./post-audio.html">
-											{" "}
-											post audio
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./post-gallery.html">
-											{" "}
-											post gallery
-										</a>{" "}
-									</li>
-									<li>
-										<a className="dropdown-item" href="./post-no-sidebar.html">
-											{" "}
-											post no sidebar{" "}
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./post-left-sidebar.html">
-											{" "}
-											post left sidebar{" "}
-										</a>
-									</li>
-								</ul>
-							</li>
-
-							<li className="nav-item dropdown">
-								<a className="nav-link  dropdown-toggle" href="./#" data-toggle="dropdown">
-									Pages{" "}
-								</a>
-								<ul className="dropdown-menu fade-up">
-									<li>
-										<a className="dropdown-item" href="./author.html">
-											{" "}
-											author{" "}
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="./page404.html">
-											{" "}
-											Page 404{" "}
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="./contact.html">
-									{" "}
-									Contact{" "}
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="./about.html">
-									{" "}
-									About{" "}
-								</a>
-							</li>
+							{linkData.map(({ id, path, linkTag }) => (
+								<li key={id} className="nav-item">
+									<NavLink className={"nav-link"} to={path}>
+										{linkTag}
+									</NavLink>
+								</li>
+							))}
 						</ul>
 					</div>
-					{/* <!--/--> */}
 
-					{/* <!--navbar-right--> */}
+					{/* NAVBAR RIGHT */}
 					<div className="navbar-right ml-auto">
 						<div className="theme-switch-wrapper">
 							<label className="theme-switch" htmlFor="checkbox">
@@ -212,52 +78,36 @@ const Navigation = () => {
 						{/* <!--social-media--> */}
 						<div className="social-icones">
 							<ul className="list-inline">
-								<li>
-									<a href="./#">
-										{" "}
-										<i className="fab fa-facebook-f"></i>
-									</a>
-								</li>
-								<li>
-									<a href="./#">
-										<i className="fab fa-instagram"></i>
-									</a>{" "}
-								</li>
-								<li>
-									<a href="./#">
-										<i className="fab fa-twitter"></i>
-									</a>
-								</li>
-								<li>
-									<a href="./#">
-										<i className="fab fa-youtube"></i>
-									</a>
-								</li>
+								{socialLinks.map(({ socialName, iconTag, path }) => (
+									<li key={socialName}>
+										<NavLink to={path}>
+											<i className={iconTag} />
+										</NavLink>
+									</li>
+								))}
 							</ul>
 						</div>
-						{/* <!--search-icon--> */}
+						{/* SEARCH ICON */}
 						<div className="search-icon">
 							<i className="icon_search"></i>
 						</div>
 
-						{/* <!--login-block--> */}
+						{/* LOGIN BLOCK */}
 						<div className="login-block">
-							<a className="login-icon" href="./#">
+							<span className="login-icon">
 								<i className="icon_profile"></i>
-							</a>
+							</span>
 							<ul className="login-menu fade-up">
 								<li>
-									{" "}
-									<a href="./login.html">login </a>{" "}
+									<NavLink to="login">login</NavLink>
 								</li>
 								<li>
-									{" "}
-									<a href="./signup.html">sign up</a>{" "}
+									<NavLink to="signup">sign up</NavLink>
 								</li>
 							</ul>
 						</div>
 
-						{/* <!--navbar-toggler--> */}
+						{/* NAVBAR TOGGLER */}
 						<button
 							className="navbar-toggler"
 							type="button"
